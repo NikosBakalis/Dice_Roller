a156feafc09e4f5a22a143a0a6f7a6e2738a7450
Nikos Bakalis <47317522+NikosBakalis@users.noreply.github.com>
2019-03-25 15:32:58 +0200

    Update DiceRoller.java

--- Program-IntelliJ/DnD-Dice-Roller/src/DiceRoller.java
+++ Program-IntelliJ/DnD-Dice-Roller/src/DiceRoller.java
@@ -55,6 +55,8 @@ public class DiceRoller {
                     default:
                         break;
                 }
+            }else{
+                continue;
             }
         }
     }
